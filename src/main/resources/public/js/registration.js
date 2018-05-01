7b08d2f3b6d65be6b130c112d40dfbdc67c19ef4
Parviz Makari <parvizmakarti@gmail.com>
2018-05-01 14:53:46 +0200

    Removed comments from registration JS, and added client side validations

--- src/main/resources/public/js/registration.js
+++ src/main/resources/public/js/registration.js
@@ -1,45 +1,27 @@
 /**
  * Created by Parviz on 29.04.2018.
  */
-// Variable to hold request
 var request;
-function getFormData($form){
+function getFormData($form) {
     var unindexed_array = $form.serializeArray();
     var indexed_array = {};
 
-    $.map(unindexed_array, function(n, i){
+    $.map(unindexed_array, function (n, i) {
         indexed_array[n['name']] = n['value'];
     });
 
     return indexed_array;
 }
-
-// Bind to the submit event of our form
-$("#regForm").submit(function(event){
-
-    // Prevent default posting of form - put here to work in case of errors
+$("#regForm").submit(function (event) {
     event.preventDefault();
 
-    // Abort any pending request
     if (request) {
         request.abort();
     }
-    // setup some local variables
     var $form = $(this);
-
-    // Let's select and cache all the fields
     var $inputs = $form.find("input, select, button, textarea");
-
-    // Serialize the data in the form
-    // var serializedData = $("#regForm").serializeToString();
-    // var data = JSON.stringify( $("#regForm").serializeArray() ); //  <-----------
     var $formData = $("#regForm");
     var data = JSON.stringify(getFormData($formData));
-
-    console.log(data);
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
     $inputs.prop("disabled", true);
     request = $.ajax({
         headers: {
@@ -51,46 +33,35 @@ $("#regForm").submit(function(event){
         data: data,
         dataType: 'json'
     });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
+    request.done(function (response, textStatus, jqXHR) {
         $('#response').addClass('alert alert-success').html(response.message);
         $('#regForm').trigger("reset");
         $('#regForm').hide()
         $('#panelMsg').empty();
         $('#panelMsg').text("Your Entered Data");
         $('#responseData').show();
-        $('#responseData ul').append('<li>Name : '+response.name+'</li>');
-        $('#responseData ul').append('<li>Email :' +response.email+'</li>');
-        if (response.phoneNumber!="") {
+        $('#responseData ul').append('<li>Name : ' + response.name + '</li>');
+        $('#responseData ul').append('<li>Email :' + response.email + '</li>');
+        if (response.phoneNumber != "") {
             $('#responseData ul').append('<li>Phone Number : ' + response.phoneNumber + '</li>');
         }
-        $('#responseData ul').append('<li>Address : '+response.country+'</li>');
+        $('#responseData ul').append('<li>Address : ' + response.country + '</li>');
         console.log(response);
     });
 
-    // Callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        if(jqXHR.status == 400){
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+        if (jqXHR.status == 400) {
             $('#response').addClass('alert alert-danger');
             $('#response').empty();
             var data = JSON.parse(jqXHR.responseText);
             $.each(data, function (index, row) {
-                $('#response').append('<h6>'+row.message+'</h6>');
+                $('#response').append('<h6><b>' + row.field + '</b> ' + row.message + '</h6>');
             });
-
-        }else
-        {
+        } else {
             $('#response').addClass('alert alert-danger').html(jqXHR.responseText);
         }
     });
-
-    // Callback handler that will be called regardless
-    // if the request failed or succeeded
     request.always(function () {
-        // Reenable the inputs
         $inputs.prop("disabled", false);
     });
 
